b588c7ad60e721a90c3dd08f0b4a93266f21b9ff
tbalint19 <toth910719balint@gmail.com>
2016-09-01 10:56:42 +0200

    Delete maintenance.py

--- maintenance.py
+++
@@ -1,9 +0,0 @@
-from applicant import *
-from city import *
-
-
-# adds unique applicaton codes to those who doesn't have
-Applicant.generate_unique(Applicant.find_missing_app_num())
-
-# finds the closest school based on the city where the applicant lives (for everyone)
-Applicant.find_school(Applicant.find_missing_city())

79b710d13dfde39347f9d63e0e37f55c2ab2242f
vogronicspatrik <vogronicspatrik@gmail.com>
2016-09-01 13:42:33 +0200

    import repaired

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
 from inputhandler import *
 
 app = Flask(__name__)
@@ -7,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/registration', methods=['GET', 'POST'])
 def register():
-    if requests.methods = 'POST':
-        all_data = requests.form
+    if request.methods = 'POST':
+        all_data = request.form
         all_info = check_inputs(all_data)
         return render_template('/register.html', data=all_info)
 

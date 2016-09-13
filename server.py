5b5df43789b203dc30de7f1656d382d0f316d831
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-09-13 14:15:50 +0200

    login and welcome app routes

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request
-from inputhandler import *
+from flask import Flask, render_template, request, session, redirect, url_for, g
 from model_applicant import *
 from model_city import *
 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.secret_key = 'pornislife'
+
+@app.before_request
+def login_user():
+    g.user = None
+    if 'user' in session:
+        g.user = session['application_number']
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -15,24 +22,30 @@ def register():
             'Email': request.form['Email'],
             'Name': request.form['Name']
         }
-        check_results = Inputhandler.check_inputs(all_data)
-        if not check_results:
+        if not Applicant.check_if_email_is_used():
             city = City.get_by_name(all_data['City'])
             Applicant.create(name=all_data['Name'], city=city, email=all_data['Email'])
-            return redirect(url_for...)
-    else:
-        check_results = False
-    return render_template('/register.html', data=check_results)
+
+            return redirect(url_for(welcome, code=307))
+    return render_template('/register.html', data=all_data )
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return render_template("welcome.html")
+    successful = True
+    if request.method == 'GET':
+        successful = False
+    return render_template("welcome.html", after_successfull=successful)
 
 
-@app.route('/applicant/login', methods=['GET, POST'])
+@app.route('/applicant/login', methods=['GET', 'POST'])
 def login():
-    pass
+    if request.method == 'POST':
+        user = Applicant.find_by_appnum(request.form['password'])
+        if user.email == request.form['email']:
+            session['application_number'] = user.application_number
+            return redirect((url_for(status_of_application)))
+    return render_template(url_for(login))
 
 
 @app.route('/applicant/profile', methods=['GET, POST'])
@@ -48,3 +61,11 @@ def status_of_interviews():
 @app.route('/about_us', methods=['GET, POST'])
 def route_to_about_us():
     return render_template("about_us.html")
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('application_number', None)
+    redirect(url_for(welcome))
+
+
